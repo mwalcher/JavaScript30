3cc55f8a34eed98a776f1cc16c13625c67af9535
Matt Walcher <matt@mwalcher.com>
2021-02-03 21:33:23 -0500

    added red effect filter

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -22,7 +22,10 @@ function paintToCanvas() {
   canvas.height = height;
 
   return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height);
+      ctx.drawImage(video, 0, 0, width, height);
+      let pixels = ctx.getImageData(0, 0, width, height);
+      pixels = redEffect(pixels);
+      ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -38,6 +41,15 @@ function takePhoto() {
   link.setAttribute('download', 'photo');
   link.innerHTML = `<img src="${data}" alt="" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 100; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
 }
 
 getVideo();
